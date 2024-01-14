5b1f0940e0ca23a50b378988178e8c946ce62c75
root <root@vitalii>
2024-01-15 00:10:10 +0200

    added VideoHTMLAttributes playsInline=True

--- app.py
+++ app.py
@@ -1,145 +1,26 @@
-import logging
-import queue
-from pathlib import Path
-from typing import List, NamedTuple
-
-import av
-import cv2
-import numpy as np
 import streamlit as st
-from streamlit_webrtc import WebRtcMode, webrtc_streamer
-#from helper import load_model
-import os
-#from turn import get_ice_servers
-#from ultralytics import YOLO
-#import torch
-
-#st.write(f"CUDA {torch.cuda.is_available()}")
-
-HERE = Path(__file__).parent
-ROOT = HERE.parent
-
-logger = logging.getLogger(__name__)
+from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc.config import VideoHTMLAttributes
+import av
 
 
-MODEL_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.caffemodel"  # noqa: E501
-MODEL_LOCAL_PATH = ROOT / "./models/MobileNetSSD_deploy.caffemodel"
-PROTOTXT_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.prototxt.txt"  # noqa: E501
-PROTOTXT_LOCAL_PATH = ROOT / "./models/MobileNetSSD_deploy.prototxt.txt"
-
-weights_dir = "weights"
-yolov8_nano_path = os.path.join(weights_dir, "yolov8n.pt")
-
-CLASSES = ["person"]
+flip = st.checkbox("Flip")
 
 
-class Detection(NamedTuple):
-    class_id: int
-    label: str
-    score: float
-    box: np.ndarray
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    flipped = img[::-1,:,:] if flip else img
+
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
 
-@st.cache_resource  # type: ignore
-def generate_label_colors():
-    return np.random.uniform(0, 255, size=(len(CLASSES), 3))
-
-
-COLORS = generate_label_colors()
-
-
-# Session-specific caching
-'''
-cache_key = "object_detection"
-if cache_key in st.session_state:
-    model = st.session_state[cache_key]
-    print("model", type(model))
-else:
-    print("="*10)
-    #model = load_model(yolov8_nano_path)
-    model = YOLO("yolov8n.pt")
-    st.session_state[cache_key] = model
-    print(type(st.session_state[cache_key]))
-'''
-score_threshold = st.slider("Score threshold", 0.0, 1.0, 0.5, 0.05)
-
-# NOTE: The callback will be called in another thread,
-#       so use a queue here for thread-safety to pass the data
-#       from inside to outside the callback.
-# TODO: A general-purpose shared state object may be more useful.
-result_queue: "queue.Queue[List[Detection]]" = queue.Queue()
-
-
-def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    image = frame.to_ndarray(format="bgr24")
-
-    '''
-    results = model(image)
-    annotated_frame = results[0].plot()
-    image = annotated_frame
-    print("image", image.shape)
-
-    
-    detections = [
-        Detection(
-            class_id=int(detection[1]),
-            label=CLASSES[int(detection[1])],
-            score=float(detection[2]),
-            box=(detection[3:7] * np.array([w, h, w, h])),
-        )
-        for detection in output
-    ]
-
-    # Render bounding boxes and captions
-    for detection in detections:
-        caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
-        color = COLORS[detection.class_id]
-        xmin, ymin, xmax, ymax = detection.box.astype("int")
-
-        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-        cv2.putText(
-            image,
-            caption,
-            (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            color,
-            2,
-        )
-    
-    result_queue.put(detections)
-    ''' 
-    result_queue.put(["person", "vehicle"])
-
-    return av.VideoFrame.from_ndarray(image, format="bgr24")
-
-
-webrtc_ctx = webrtc_streamer(
-    key="object-detection",
-    mode=WebRtcMode.SENDRECV,
-    #rtc_configuration=get_ice_servers(),
-    rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    },
-    video_frame_callback=video_frame_callback,
-    media_stream_constraints={"video": True, "audio": False},
-    async_processing=True,
-)
-
-if st.checkbox("Show the detected labels", value=True):
-    if webrtc_ctx.state.playing:
-        labels_placeholder = st.empty()
-        # NOTE: The video transformation with object detection and
-        # this loop displaying the result labels are running
-        # in different threads asynchronously.
-        # Then the rendered video frames and the labels displayed here
-        # are not strictly synchronized.
-        while True:
-            result = result_queue.get()
-            labels_placeholder.table(result)
-
-st.markdown(
-    "This demo uses a model and code from "
-    "https://github.com/robmarkcole/object-detection-app. "
-    "Many thanks to the project."
+webrtc_streamer(key="example", 
+                video_frame_callback=video_frame_callback, 
+                video_html_attrs=VideoHTMLAttributes(
+                    autoPlay=True, 
+                    controls=True, 
+                    style={"width": "100%"},
+                    playsInline=True
+                )
 )
